4642e5e06627533bfa0f1989e1f818970e0c29bf
Guscelleguim <gugacelleguim@gmail.com>
2024-04-25 17:36:07 -0300

    first commit

---
+++ src/data/articles.js
@@ -0,0 +1,264 @@
+import React from "react";
+
+function article_1() {
+	return {
+		date: "Feb 7, 2024",
+		title: "Revolutionizing Web Development : The Unstoppable Impact of React ",
+		description:
+			"CIf you are delving into the realm of software development, you’ve likely come across React. But what exactly is this framework, and why is it so widely popular among developers?",
+		keywords: [
+			"The Benefits of Cloud Computing",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*PPtMax3hSeMm-l6EhLXmRg.png" alt="random" className="randImage"/>
+				</div>		
+
+				<p>
+					If you are delving into the realm of software development, you’ve likely come across React. But what exactly is this framework, and why is it so widely popular among developers?
+				</p>
+
+				<h2>Why is React Popular in Programming?</h2>
+
+				<p>React JS is an open-source JavaScript library, focused exclusively on the view layer. Developed by Facebook, it excels in building modern web user interfaces (UIs), enabling the creation of reusable components that enhance modularity and ease maintenance.</p>
+
+				<h3>React JS: A Library for User Interfaces </h3>
+				<p> React JS is an open-source JavaScript library, focused exclusively on the view layer. Developed by Facebook, it excels in building modern web user interfaces (UIs), enabling the creation of reusable components that enhance modularity and ease maintenance.</p>
+
+				<h4>he Significance of React JS in Development</h4>
+				<p>React JS is renowned for its efficiency and performance, largely due to the Virtual DOM, an algorithm that optimizes interface updates, significantly improving user experience. Moreover, React promotes a declarative approach to programming, simplifying the development process by abstracting direct DOM manipulation, making the code more intuitive and accessible.</p>
+
+				<h4>How React JS Works and Its Contribution to Web Development</h4>
+
+				<p>
+				React operates through component composition: independent and reusable code blocks that constitute the UI. Key elements in this context are state and properties (props), which dynamize and customize components. React’s applicability extends from web to mobile development with React Native, compatible with most browsers and integrable with other libraries and frameworks, showcasing its versatility and scalability.
+				</p>
+
+				<h5>**The Philosophy of React JS: Simplicity and Efficiency**</h5>
+
+				<p>
+					React is distinguished by its simplicity and focus on code reuse, elements that lead to more agile and productive development. The selective updating of the DOM, enabled by the Virtual DOM, optimizes performance and enhances the user experience. This minimalist approach ensures cleaner, easier-to-understand, and maintainable code.
+				</p>
+
+				<h5>Applying React JS in Projects and Optimizing Programming</h5>
+
+				<p>To implement React JS in projects, basic knowledge of JavaScript and web programming is required. It’s also recommended to learn concepts such as state, properties, and component lifecycle. There are various online resources, like official documentation and tutorials, to assist in learning React JS.</p>
+
+				<p>
+					By using React JS, programming can be optimized in several ways. The modularity of components allows for better code organization, making it more readable and maintainable. Furthermore, the use of the Virtual DOM and selective interface updating significantly reduces rendering time and improves user experience.
+
+					Additional optimization can be achieved by using complementary libraries and frameworks, such as Redux for state management or Next.js for server-side rendering. These tools can extend React JS functionalities and further enhance web application development.
+
+					In summary, React JS is a powerful tool in web development, enabling the creation of modern and efficient interfaces. Its significance in programming is evident, and its philosophy of simplicity and code reuse makes development more agile and productive. By applying React JS to projects and optimizing programming, developers can offer a superior user experience and achieve high-quality results. 
+				</p>
+
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "7 May 2023",
+		title: "",
+		description:
+			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+
+				<div className="article-content">
+					<img src="https://media.licdn.com/dms/image/D4D12AQGvyasrU7PPWw/article-cover_image-shrink_720_1280/0/1708697805911?e=1719446400&v=beta&t=46_o8Hb5PxqSEkDAq1x-tDrB9UH9hyyB4UIJlsrrHRU" alt="random" className="randImage"/>
+				</div>		
+
+				<h1> My Experience with Obsidian and How It's Revolutionizing My Personal Projects </h1>
+
+				<p>O Obsidian é um aplicativo revolucionário que atua como um "segundo cérebro", funcionando localmente através de textos em formato Markdown. Essa característica assegura uma fácil migração e compatibilidade com outros aplicativos que suportam esse formato. Muitos aplicativos de anotações enfrentam o problema de perder informações com o acúmulo de notas. O Obsidian, com sua metodologia de "segundo cérebro", não apenas armazena informações, mas também lembra constantemente o usuário delas, criando uma rede neural de informações.</p>
+
+				<h2>Como o Obsidian Funciona</h2>
+
+				<p>O Obsidian opera como um editor de Markdown padrão, permitindo a criação de pastas, notas e a utilização de atalhos para editar texto. Ele suporta "CommonMark" e "GitHub Flavored Markdown", garantindo ampla compatibilidade com outros editores de Markdown. O diferencial do Obsidian está em sua capacidade de criar links entre arquivos, formando assim o seu "segundo cérebro". Basta adicionar "[[" e começar a digitar o nome do arquivo que deseja conectar.</p>
+
+				<p>
+					Além disso, o Obsidian suporta a abertura de múltiplos arquivos simultaneamente, ideal para quem possui um monitor grande e deseja maximizar o uso do espaço. Você pode abrir, organizar e redimensionar quantos arquivos quiser.
+					Existem diversos plugins que expandem as possibilidades do Obsidian, incluindo
+				</p>
+
+				<ol>
+
+					<li>
+						Preview: Visualize páginas passando o mouse sobre o link (funciona apenas no modo Preview).
+					</li>
+
+					<li>
+						Starred Notes: Marque páginas usadas frequentemente para acesso rápido.
+					</li>
+
+					<li>
+						Custom CSS: Personalize a interface do Obsidian, alterando cores, fontes e outros aspectos.
+					</li>
+					
+					<li>
+						Audio Recorder: Grave áudios diretamente no Obsidian.
+					</li>
+
+                </ol>
+
+			<h3>Por Que Estou Usando o Obsidian e Sua Utilidade em Meus Projetos</h3>
+
+			<p>No desenvolvimento do projeto Absoluto, uma escola particular de tecnologia com aplicações front-end e back-end robustas, o Obsidian se mostrou uma ferramenta essencial. Ele facilitou a organização de snippets de código e soluções específicas, além de oferecer uma maneira intuitiva de revisitar e refinar essas ideias. O Obsidian se tornou o principal meio de documentação para o projeto, abrangendo aspectos de front-end e back-end, permitindo a construção de uma base de conhecimento coesa.</p>
+			
+			<h4>
+				Impacto no Projeto
+			</h4>
+
+			<p>
+				A adoção do Obsidian transformou a gestão e o acesso a informações no projeto Absoluto. A facilidade de acesso a informações relevantes agilizou o processo de tomada de decisões e implementação de funcionalidades, mantendo a visão do projeto alinhada com os objetivos iniciais.
+			</p>
+
+	<h5>
+		Comparação com Outras Ferramentas
+	</h5>
+
+	<ol>
+		<li>
+			Obsidian: Se destaca pela flexibilidade na organização de notas e pela capacidade de criar uma base de conhecimento interconectada. Oferece uma experiência altamente personalizável com uma ampla gama de plugins e temas.	
+		</li>
+
+		<li>
+		Evernote: Conhecido por sua sincronização em nuvem e ferramentas avançadas de captura de informações. Embora ofereça uma boa estrutura de organização, pode não ser tão flexível quanto o Obsidian para projetos complexos.
+		</li>
+
+		<li>
+			Notion: Uma ferramenta tudo-em-um que combina notas, tarefas, bancos de dados e wikis. Excelente para colaboração em equipe, mas pode não oferecer a mesma flexibilidade de personalização que o Obsidian
+		</li>
+
+	</ol>
+
+		<h5>
+			Conclusão
+		</h5>
+
+		<p>
+			O Obsidian se mostrou mais do que um simples aplicativo de anotações para o projeto Absoluto; tornou-se uma ferramenta fundamental no processo de desenvolvimento, facilitando a gestão do conhecimento e a documentação de forma eficiente. Para desenvolvedores e designers trabalhando em projetos complexos, o Obsidian representa uma solução poderosa para manter tudo organizado e acessível. 
+		</p>
+
+		<ol>
+			
+		<li a href ="https://www.youtube.com/watch?v=BgzRGat8WfE">
+			Obsidian para Iniciantes - Vladimir Campos (recomendo o canal dele ensina bastante sobre a ferramenta)
+		</li>
+
+		< li a href="https://www.youtube.com/@lucas_badico"> 
+			Evernote: Conhecido por sua sincronização em nuvem e ferramentas avançadas de captura de informações. Embora ofereça uma boa estrutura de organização, pode não ser tão flexível quanto o Obsidian para projetos complexos.
+		</li>
+
+		<li a href = "https://www.youtube.com/watch?v=y4cVQgmEMqs" >
+			Notion: Uma ferramenta tudo-em-um que combina notas, tarefas, bancos de dados e wikis. Excelente para colaboração em equipe, mas pode não oferecer a mesma flexibilidade de personalização que o Obsidian
+		</li>
+
+	</ol>
+
+
+
+			</React.Fragment>
+		),
+	};
+}
+
+function article_3() {
+	return {
+		date: "7 May 2023",
+		title: "",
+		description:
+			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+
+				<div className="article-content">
+					<img src="https://media.licdn.com/dms/image/D4D12AQGvyasrU7PPWw/article-cover_image-shrink_720_1280/0/1708697805911?e=1719446400&v=beta&t=46_o8Hb5PxqSEkDAq1x-tDrB9UH9hyyB4UIJlsrrHRU" alt="random" className="randImage"/>
+				</div>		
+
+				<h1> My Experience with Obsidian and How It's Revolutionizing My Personal Projects </h1>
+
+				<p>O Obsidian é um aplicativo revolucionário que atua como um "segundo cérebro", funcionando localmente através de textos em formato Markdown. Essa característica assegura uma fácil migração e compatibilidade com outros aplicativos que suportam esse formato. Muitos aplicativos de anotações enfrentam o problema de perder informações com o acúmulo de notas. O Obsidian, com sua metodologia de "segundo cérebro", não apenas armazena informações, mas também lembra constantemente o usuário delas, criando uma rede neural de informações.</p>
+
+				<h2>Como o Obsidian Funciona</h2>
+
+				<p>O Obsidian opera como um editor de Markdown padrão, permitindo a criação de pastas, notas e a utilização de atalhos para editar texto. Ele suporta "CommonMark" e "GitHub Flavored Markdown", garantindo ampla compatibilidade com outros editores de Markdown. O diferencial do Obsidian está em sua capacidade de criar links entre arquivos, formando assim o seu "segundo cérebro". Basta adicionar "[[" e começar a digitar o nome do arquivo que deseja conectar.</p>
+
+				<p>
+					Além disso, o Obsidian suporta a abertura de múltiplos arquivos simultaneamente, ideal para quem possui um monitor grande e deseja maximizar o uso do espaço. Você pode abrir, organizar e redimensionar quantos arquivos quiser.
+					Existem diversos plugins que expandem as possibilidades do Obsidian, incluindo
+				</p>
+
+				<ol>
+
+					<li>
+						Preview: Visualize páginas passando o mouse sobre o link (funciona apenas no modo Preview).
+					</li>
+
+					<li>
+						Starred Notes: Marque páginas usadas frequentemente para acesso rápido.
+					</li>
+
+					<li>
+						Custom CSS: Personalize a interface do Obsidian, alterando cores, fontes e outros aspectos.
+					</li>
+					
+					<li>
+						Audio Recorder: Grave áudios diretamente no Obsidian.
+					</li>
+
+				</ol>
+
+			<h3>Por Que Estou Usando o Obsidian e Sua Utilidade em Meus Projetos</h3>
+
+			<p>No desenvolvimento do projeto Absoluto, uma escola particular de tecnologia com aplicações front-end e back-end robustas, o Obsidian se mostrou uma ferramenta essencial. Ele facilitou a organização de snippets de código e soluções específicas, além de oferecer uma maneira intuitiva de revisitar e refinar essas ideias. O Obsidian se tornou o principal meio de documentação para o projeto, abrangendo aspectos de front-end e back-end, permitindo a construção de uma base de conhecimento coesa.</p>
+			
+			<h4>
+				Impacto no Projeto
+			</h4>
+
+			<p>
+				A adoção do Obsidian transformou a gestão e o acesso a informações no projeto Absoluto. A facilidade de acesso a informações relevantes agilizou o processo de tomada de decisões e implementação de funcionalidades, mantendo a visão do projeto alinhada com os objetivos iniciais.
+			</p>
+
+			</React.Fragment>
+		),
+	};
+}
+
+
+const myArticles = [article_1, article_2 ,article_3];
+
+export default myArticles;
---
+++ src/data/user.js
@@ -0,0 +1,102 @@
+const INFO = {
+	main: {
+		title: "Gustavo Celleguim",
+		name: "Gustavo Celleguim",
+		email: "gugacelleguim@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/phptatavo",
+		github: "https://github.com/GusCelleguim",
+		linkedin: "https://www.linkedin.com/in/gustavocelleguim/",
+		instagram: "https://www.instagram.com/guscelleguim_/",
+		stackoverflow: "https://stackoverflow.com/users/20635018/gustavo-de-andrade-celleguim",
+		facebook: "https://www.facebook.com/gustavinho.zo.1/",
+	},
+
+	homepage: {
+		title: " Expert in Full-Stack Web & Mobile Development, Tech Influencer ",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+
+		title: "I’m Gustavo de Andrade Celleguim . I live in Brasil 🇧🇷 , where I design system the future.",
+
+		description:
+			"I am a developer with a deep passion for design, and I have gained considerable knowledge from books and courses. One particularly impactful course was taught by Felipe Santana, which provided me with numerous innovative ideas and projects. I am continuously studying to refine my skills because I believe that all knowledge is like a masterpiece in progress. Currently, I am reading books on design concepts, always striving to enhance my expertise and bring my projects to the next level.",
+	},
+
+	articles: {
+
+		title:"",
+		description:"",
+
+	},
+
+	projects: [
+
+		{
+			title: "Jornada do heroi  - Etec Project ",
+			description:
+				"O projeto Jornada do Herói :é uma iniciativa de educação em desenvolvimento de sistemas, focando em desenvolvimento web, back-end e DevOps. Seu objetivo é fornecer um caminho de aprendizado completo através de documentações detalhadas, videoaulas, e workshops, abrangendo desde fundamentos de programação até práticas avançadas.",
+			logo: "https://w7.pngwing.com/pngs/515/44/png-transparent-superhero-silhouette-superman-superhero-silhouette-heros-comics-heroes-hand-thumbnail.png",
+			linkText: "Conheça esta jornada",
+			link: "https://github.com/GusCelleguim/Jornada-do-Heroi"
+		},
+
+
+		{
+			title: "Code Academy - Entendendo o HTML",
+			description: "" ,
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "code academy my projects student",
+			link: "https://github.com",
+		},
+
+
+		{
+			title: "Digital INOVETION ONE - SPIDER-MEN ",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "Conheça O Projeto",
+			link: "https://github.com",
+		},
+
+
+// Academia Physical
+		{
+			title: "Academia Physical",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+// Plataforma dos alunos Etec - ETEC Dr. Emílio Hernandez Aguilar 
+		{
+			title: "em andamento ",
+			description:
+				"",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "",
+		},
+
+//  Projeto da plataforma de ensino..
+		{
+			title: "EM Andamento...",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
